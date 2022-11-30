378dbf4d96b701d87637464fcbd9f11195f10777
Gustavo Murayama <gmurayama@outlook.com>
2022-11-30 00:02:43 -0300

    api implementation

--- gateways/api/src/main.rs
+++ gateways/api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod server;
+
+use std::net::TcpListener;
+
+use server::setup_server;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7000")?;
+    let server = setup_server(listener).await?;
+    server.await?;
+
+    Ok(())
 }
---
+++ gateways/api/src/server.rs
@@ -0,0 +1,15 @@
+use actix_web::{dev::Server, get, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+pub async fn setup_server(listener: TcpListener) -> eyre::Result<Server> {
+    let server = HttpServer::new(|| App::new().service(hello))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
+}
