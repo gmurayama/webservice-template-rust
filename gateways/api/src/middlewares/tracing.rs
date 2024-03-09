16a96133331c35809349c91532e94ea0cb9c83bc
Gustavo Murayama <gmurayama@outlook.com>
2024-03-09 18:59:00 -0300

    feat: error tag on 5xx response status

--- gateways/api/src/middlewares/tracing.rs
+++ gateways/api/src/middlewares/tracing.rs
@@ -11,13 +11,19 @@ impl RootSpanBuilder for LevelRootSpanBuilder {
             "/healthcheck" | "/metrics" => Level::DEBUG,
             _ => Level::INFO,
         };
-        tracing_actix_web::root_span!(level = level, request)
+        tracing_actix_web::root_span!(level = level, request, error = tracing::field::Empty)
     }
 
     fn on_request_end<B: actix_web::body::MessageBody>(
         span: Span,
         outcome: &Result<ServiceResponse<B>, Error>,
     ) {
+        if let Ok(response) = outcome {
+            if response.status().as_u16() >= 500 {
+                span.record("error", true);
+            }
+        }
+
         DefaultRootSpanBuilder::on_request_end(span, outcome);
     }
 }
